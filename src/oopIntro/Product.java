6d6809a797c82a1fcddebf236acdc76f71f9d62f
Mahmut <56754759+mahmutalann@users.noreply.github.com>
2023-08-26 09:50:20 +0300

    OOP

--- src/oopIntro/Product.java
+++ src/oopIntro/Product.java
@@ -1,12 +1,12 @@
 package oopIntro;
 
 public class Product {
-	public Product() {	//Constructor
+	public Product() {	//Creating Constructor
 		System.out.println("product constructor text");
 	}
 	
 	public Product(int id, String name, double unitPrice, String detail) {
-		this();
+		this(); //When using this function, the function equalizes to as belongs variable of the Product class' to the Product constructor block's variable.
 		this.id = id;
 		this.name = name;
 		this.unitPrice = unitPrice;
